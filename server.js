d3dbb75f2d737d1561d4c70c52efb700f7d92db6
sweeneyian <sweeney.ian@gmail.com>
2019-11-12 21:24:28 +0000

    socket io and node static

--- server.js
+++ server.js
@@ -3,4 +3,11 @@ const app = express()
 
 app.use(express.static('public'))
 
-app.listen(process.env.PORT || 8080, ()=> console.log('Online'))
+app.listen(process.env.PORT || 8080, ()=> console.log('Server Up'))
+
+'use strict';
+
+var os = require('os');
+var nodeStatic = require('node-static');
+var http = require('http');
+var socketIO = require('socket.io');
