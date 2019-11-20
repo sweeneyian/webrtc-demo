5850d123841608a2ff8b859cd8931dd672b8a7ab
sweeneyian <sweeney.ian@gmail.com>
2019-11-20 19:43:05 +0000

    housekeeping changes

--- public/js/main.js
+++ public/js/main.js
@@ -22,6 +22,7 @@ const localVideo = document.getElementById('localVideo');
 const localAudio = document.getElementById('localAudio');
 const remoteVideo = document.getElementById('remoteVideo');
 const remoteAudio = document.getElementById('remoteAudio');
+const sendReceive = document.getElementById('sendReceive');
 
 // Peer variables
 
@@ -45,7 +46,7 @@ sendButton.onclick = sendData;
 
 
 
-//////// Audio Events
+//////// Audio Spectrogram Event Handling
 
 remoteVideo.addEventListener('loadedmetadata', () => {
   return console.log(`Remote video videoWidth: ${this.videoWidth}px,  videoHeight: ${this.videoHeight}px`);
@@ -83,73 +84,32 @@ function call() {
   startTime = window.performance.now();
   isInitiator = true;
   callButton.disabled = true;
-  upgradeButton.disabled = false;
   hangupButton.disabled = false;
   sendMessage('calling' + room);
   maybeStart();
-  // const audioTracks = localStream.getAudioTracks();
-  // if (audioTracks.length > 0) {
-  //   console.log(`Using audio device: ${audioTracks[0].label}`);
-  // }
-  // const servers = null;
-  // pc1 = new RTCPeerConnection(servers);
-  // console.log('Created local peer connection object pc1');
-  // pc1.onicecandidate = e => onIceCandidate(pc1, e);
-  // pc2 = new RTCPeerConnection(servers);
-  // console.log('Created remote peer connection object pc2');
-  // pc2.onicecandidate = e => onIceCandidate(pc2, e);
-  // pc1.oniceconnectionstatechange = e => onIceStateChange(pc1, e);
-  // pc2.oniceconnectionstatechange = e => onIceStateChange(pc2, e);
-  // pc2.ontrack = gotRemoteStream;
-
-  // localStream.getTracks().forEach(track => pc1.addTrack(track, localStream));
-  // console.log('Added local stream to pc1');
-
-  // console.log('pc1 createOffer start');
-  // pc1.createOffer(offerOptions).then(onCreateOfferSuccess, onCreateSessionDescriptionError);
 }
 
 
 function upgrade() {
-  // upgradeButton.disabled = true;
-  // navigator.mediaDevices
-  //   .getUserMedia({video: true})
-  //   .then(stream => {
-  //     const videoTracks = stream.getVideoTracks();
-  //     if (videoTracks.length > 0) {
-  //       console.log(`Using video device: ${videoTracks[0].label}`);
-  //     }
-  //     localStream.addTrack(videoTracks[0]);
-  //     localVideo.srcObject = null;
-  //     localVideo.srcObject = localStream;
-  //     pc1.addTrack(videoTracks[0], localStream);
-  //     return pc1.createOffer();
-  //   })
-  //   .then(offer => pc1.setLocalDescription(offer))
-  //   .then(() => pc2.setRemoteDescription(pc1.localDescription))
-  //   .then(() => pc2.createAnswer())
-  //   .then(answer => pc2.setLocalDescription(answer))
-  //   .then(() => pc1.setRemoteDescription(pc2.localDescription));
+  upgradeButton.disabled = true;
+  localVideo.style.display = '';
+  navigator.mediaDevices
+    .getUserMedia({video: true})
+    .then(stream => {
+      const videoTracks = stream.getVideoTracks();
+      if (videoTracks.length > 0) {
+        console.log(`Using video device: ${videoTracks[0].label}`);
+      }
+      localStream.addTrack(videoTracks[0]);
+      localVideo.srcObject = null;
+      localVideo.srcObject = localStream;
+      if (pc){
+        pc.addTrack(videoTracks[0], localStream);
+        return pc.createOffer(setLocalAndSendMessage, handleCreateOfferError);
+      }
+    })
 }
 
-// function hangup() {
-//   console.log('Ending call');
-//   pc1.close();
-//   pc2.close();
-//   pc1 = null;
-//   pc2 = null;
-
-//   const videoTracks = localStream.getVideoTracks();
-//   videoTracks.forEach(videoTrack => {
-//     videoTrack.stop();
-//     localStream.removeTrack(videoTrack);
-//   });
-//   localVideo.srcObject = null;
-//   localVideo.srcObject = localStream;
-
-//   hangupButton.disabled = true;
-//   callButton.disabled = false;
-// }
 /////////////////////////////////////////////
 
 var socket = io.connect();
@@ -162,14 +122,14 @@ function joinRoom(){
   navigator.mediaDevices
     .getUserMedia({
       audio: true,
-      video: true
+      video: false
     })
     .then(gotStream)
     .catch(e => alert(`getUserMedia() error: ${e.name}`));
   room = roomName.value;
   console.log("Joining Room: " + room);
   roomInput.style.display = "none";
-  localVideo.style.display = '';
+  localAudio.style.display = '';
   roomDiv.innerHTML = "Welcome to <b>" + room + "</b> room" +"<br />";
 
   if (room !== '') {
@@ -178,6 +138,7 @@ function joinRoom(){
     callButton.style.display = '';
     hangupButton.style.display = '';
     upgradeButton.style.display = '';
+    upgradeButton.disabled = false;
 
     socket.emit('create or join', room);
     console.log('Attempted to create or  join room', room);
@@ -201,11 +162,6 @@ socket.on('join', function (room){
   }
 });
 
-// socket.on('calling', function (room){
-//   console.log('Someone is calling the room ' + room);
-//     // isInitiator becomes true when someone tries to call
-//     isInitiator = true;
-// });
 
 socket.on('joined', function(room) {
   console.log('joined: ' + room);
@@ -256,29 +212,14 @@ socket.on('message', function(message) {
 function gotStream(stream) {
   console.log('Adding local stream.');
   localStream = stream;
-  // const audioTracks = localStream.getAudioTracks();
-  localVideo.srcObject = stream;
-  // if (audioTracks.length > 0) {
-  //   console.log(`Using Audio device: ${audioTracks[0].label}`);
-  // }
+  //localVideo.srcObject = stream;
 
   const streamVisualizer = new StreamVisualizer(stream, localAudio);
   streamVisualizer.start();
   if (isChannelReady){
     callButton.disabled = false;
   }
-  sendMessage('got user media');
-  if (isInitiator) {
-    maybeStart();
-  }
 }
-
-// Lost with regards to ICE / TURN / STUN
-// if (location.hostname !== 'localhost') {
-//   requestTurn(
-//     'https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913'
-//   );
-// }
 
 function maybeStart() {
   console.log('>>>>>>> maybeStart() ', isStarted, localStream, isChannelReady);
@@ -309,8 +250,6 @@ function createPeerConnection() {
     pc.onremovestream = handleRemoteStreamRemoved;
     console.log('Created RTCPeerConnnection');
     
-
-  
     sendChannel = pc.createDataChannel('sendDataChannel');
     console.log('Created send data channel');
 
@@ -318,11 +257,7 @@ function createPeerConnection() {
     sendChannel.onclose = onSendChannelStateChange;
   
     pc.ondatachannel = receiveChannelCallback;
-  
-    // localConnection.createOffer().then(
-    //   gotDescription1,
-    //   onCreateSessionDescriptionError
-    // );
+
     
   } catch (e) {
     console.log('Failed to create PeerConnection, exception: ' + e.message);
@@ -375,44 +310,6 @@ function onCreateSessionDescriptionError(error) {
 }
 
 
-// function gotRemoteStream(e) {
-//   console.log('gotRemoteStream', e.track, e.streams[0]);
-
-//   // reset srcObject to work around minor bugs in Chrome and Edge.
-//   remoteVideo.srcObject = null;
-//   remoteVideo.srcObject = e.streams[0];
-// }
-
-// function onCreateAnswerSuccess(desc) {
-//   console.log(`Answer from pc2:
-// ${desc.sdp}`);
-//   console.log('pc2 setLocalDescription start');
-//   pc2.setLocalDescription(desc).then(() => onSetLocalSuccess(pc2), onSetSessionDescriptionError);
-//   console.log('pc1 setRemoteDescription start');
-//   pc1.setRemoteDescription(desc).then(() => onSetRemoteSuccess(pc1), onSetSessionDescriptionError);
-// }
-
-// function onSetLocalSuccess(pc) {
-//   console.log(`${getName(pc)} setLocalDescription complete`);
-// }
-
-// function onCreateSessionDescriptionError(error) {
-//   console.log(`Failed to create session description: ${error.toString()}`);
-// }
-
-// function onSetRemoteSuccess(pc) {
-//   console.log(`${getName(pc)} setRemoteDescription complete`);
-// }
-
-// function onSetSessionDescriptionError(error) {
-//   console.log(`Failed to set session description: ${error.toString()}`);
-// }
-
-// Lost with regards to ICE / TURN / STUN
-// function requestTurn(turnURL) {
-//
-  // }
-
 function gotRemoteStream(e) {
   if (remoteVideo.srcObject !== e.streams[0]) {
     remoteVideo.srcObject = e.streams[0];
@@ -426,6 +323,7 @@ function handleRemoteStreamAdded(event) {
   console.log('Remote stream added.');
   remoteStream = event.stream;
   remoteVideo.style.display = '';
+  remoteAudio.style.display = '';
   remoteVideo.srcObject = remoteStream;
 }
 
@@ -493,22 +391,6 @@ function onReceiveChannelStateChange() {
   const readyState = receiveChannel.readyState;
   console.log(`Receive channel state is: ${readyState}`);
 }
-
-// // function gotDescription1(desc) {
-//   localConnection.setLocalDescription(desc);
-//   console.log(`Offer from localConnection\n${desc.sdp}`);
-//   localConnection.createAnswer().then(
-//     gotDescription2,
-//     onCreateSessionDescriptionError
-//   );
-// }
-
-// function gotDescription2(desc) {
-//   console.log(`Answer from remoteConnection\n${desc.sdp}`);
-//   localConnection.setRemoteDescription(desc);
-// }
-
-
 
 function onIceCandidate(event) {
   localConnection
